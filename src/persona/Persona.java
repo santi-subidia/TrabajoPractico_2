37f2517e1bd6b6ea342529dadff7ae0f22cb2a7a
MartinGonL <martingontero@hotmail.com>
2024-08-19 17:14:11 -0300

    carga de persona
    falta implemetar el reloj

--- src/persona/Persona.java
+++ src/persona/Persona.java
@@ -1,14 +1,95 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package persona;
+
+import java.util.Objects;
 
 /**
  *
  * @author santi
  */
 public class Persona {
+    String nombre;
+    Integer edad;
+    Double peso;
+    Double estatura;
+
+    public Persona(String nombre, Integer edad, Double peso, Double estatura) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.peso = peso;
+        this.estatura = estatura;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Integer getEdad() {
+        return edad;
+    }
+
+    public void setEdad(Integer edad) {
+        this.edad = edad;
+    }
+
+    public Double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(Double peso) {
+        this.peso = peso;
+    }
+
+    public Double getEstatura() {
+        return estatura;
+    }
+
+    public void setEstatura(Double estatura) {
+        this.estatura = estatura;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" + "nombre=" + nombre + ", edad=" + edad + ", peso=" + peso + ", estatura=" + estatura + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 67 * hash + Objects.hashCode(this.nombre);
+        hash = 67 * hash + Objects.hashCode(this.edad);
+        hash = 67 * hash + Objects.hashCode(this.peso);
+        hash = 67 * hash + Objects.hashCode(this.estatura);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Persona other = (Persona) obj;
+        if (!Objects.equals(this.nombre, other.nombre)) {
+            return false;
+        }
+        if (!Objects.equals(this.edad, other.edad)) {
+            return false;
+        }
+        if (!Objects.equals(this.peso, other.peso)) {
+            return false;
+        }
+        return Objects.equals(this.estatura, other.estatura);
+    }
     
     
     
