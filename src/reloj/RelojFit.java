17e3030d7f6c442ba5eeeb25da93407e0e94a6e7
MartinGonL <martingontero@hotmail.com>
2024-08-21 18:42:07 -0300

    reloj fit
    cargo la clase de simon porque no pdia el

--- src/reloj/RelojFit.java
+++ src/reloj/RelojFit.java
@@ -1,13 +1,26 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package reloj;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Random;
 
 /**
  *
  * @author santi
  */
-public class RelojFit {
-    
+
+public class RelojFit extends Reloj {
+    private int pasos;
+
+    public RelojFit(LocalDate dia, LocalTime hora, String modelo, Long numSerie) {
+        super(dia, hora, modelo, numSerie);
+        this.pasos = 0;
+    }
+  public void cuentaPasos(int x, int y) {
+     // los pasos se suman de 1 en 1
+        this.pasos += Math.abs(x) + Math.abs(y);
+  }  
+    public void frecuenciaAleatoria() {
+        Random random = new Random();
+        int frecuencia = 60 + random.nextInt(101); // Genera una frecuencia entre 60 y 160
+        System.out.println("Frecuencia aleatoria: " + frecuencia + " bpm");  
+}
 }
